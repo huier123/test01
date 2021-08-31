1ac91341ae7106ceb70a2804db40a98daa942ceb
houyh6 <houyh6@lenovo.com>
2021-08-31 16:38:59 +0800

    add nio

---
+++ src/main/java/com/example/test01/netty/ClientHandler.java
@@ -0,0 +1,34 @@
+package com.example.test01.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.ReferenceCountUtil;
+
+@ChannelHandler.Sharable
+public class ClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            ByteBuf buf = (ByteBuf) msg;
+            byte[] data = new byte[buf.readableBytes()];
+            buf.readBytes(data);
+            System.out.println("Client：" + new String(data).trim());
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println("ClientHandler");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
